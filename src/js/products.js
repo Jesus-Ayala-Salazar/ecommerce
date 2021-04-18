e7c944771c23c31701b1667f8b16f450c2e12e45
Jesus-Ayala-Salazar <jaayala@uci.edu>
2021-04-18 16:45:29 -0700

    added links from products.html to product.html; created directory for js files

---
+++ src/js/products.js
@@ -0,0 +1,4 @@
+// let products = document.getElementsByClassName('product-img');
+// for (let i = 0; i < products.length; i++) {
+//     products[i].addEventListener('click', () => {window.location = "product.html"})
+// }
