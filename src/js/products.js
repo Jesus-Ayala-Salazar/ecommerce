a85daea62206fe31ebfd117339f4c8710b225c6a
Jesus-Ayala-Salazar <jaayala@uci.edu>
2021-04-20 02:36:55 -0700

    Responsive individual product pages; updated products.html with correct prouduct pictures; changed orange.jpg aspect ratio to fit responsive design

--- src/js/products.js
+++
@@ -1,4 +0,0 @@
-// let products = document.getElementsByClassName('product-img');
-// for (let i = 0; i < products.length; i++) {
-//     products[i].addEventListener('click', () => {window.location = "product.html"})
-// }
